b4239811707491859cc960864069752506b24cc3
Martin Fencl <martin.@layeredcode.com>
2020-04-24 16:11:26 -0500

    refactor nav elements toggle js

--- 1-tea-ui-design-project/index.js
+++ 1-tea-ui-design-project/index.js
@@ -4,12 +4,11 @@ const btnNavOpen  = get('btn-nav-open');
 const navUl       = get('nav-ul');
 const btnNavClose = get('btn-nav-close');
 
-btnNavOpen.addEventListener('click', () => {
+function toggleNav() {
   navUl.classList.toggle('show-nav-ul');
   btnNavOpen.classList.toggle('hide-btn-nav-open');
-});
+};
 
-btnNavClose.addEventListener('click', () => {
-  navUl.classList.toggle('show-nav-ul');
-  btnNavOpen.classList.toggle('hide-btn-nav-open');
-});
+btnNavOpen.addEventListener('click', () => toggleNav());
+
+btnNavClose.addEventListener('click', () => toggleNav());
