860c5e7c1738d154f6942acdc63bf23a2a62f3dd
Martin Fencl <martin.@layeredcode.com>
2020-05-19 18:31:50 -0500

    animate img-phone, with GSAP, of course

--- 3-ui-animation/index.js
+++ 3-ui-animation/index.js
@@ -4,3 +4,4 @@ let timeline = gsap.timeline(defaults)
 timeline
   .from('#wrapper-col-left', { y: -50, opacity: 0 })
   .from('.li-ul-quotes', { y: 50, opacity: 0, stagger: 0.5}, '-=.5')
+  .from('.img-phone', { y: 30, opacity: 0 }, '-=2')
