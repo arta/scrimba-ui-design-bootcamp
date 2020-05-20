8118ae9482e2e15c15f6fa855d1e85a9d6280fbf
Martin Fencl <martin.@layeredcode.com>
2020-05-19 19:00:27 -0500

    add a play GSAP animation to our a-cta
    Just to demonstrate a bit GSAP possibilities.

--- 3-ui-animation/index.js
+++ 3-ui-animation/index.js
@@ -1,7 +1,12 @@
 let defaults = { defaults: { duration: 1 } }
 let timeline = gsap.timeline(defaults)
+let aCta = document.getElementById('a-cta')
 
 timeline
   .from('#wrapper-col-left', { y: -50, opacity: 0 })
   .from('.li-ul-quotes', { y: 50, opacity: 0, stagger: 0.5}, '-=.5')
   .from('.img-phone', { y: 30, opacity: 0 }, '-=2')
+
+aCta.addEventListener('click', () => {
+  timeline.reversed() ? timeline.play() : timeline.reverse()
+})
