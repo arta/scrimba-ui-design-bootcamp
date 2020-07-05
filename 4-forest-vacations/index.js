3807b5c73d6699d95515bb21815815361ad84817
Martin Fencl <martin.@layeredcode.com>
2020-07-05 17:10:11 -0500

    add GSAP animations

--- 4-forest-vacations/index.js
+++ 4-forest-vacations/index.js
@@ -8,3 +8,13 @@ let toggleNav = () => ulNav.classList.toggle('display-nav')
 
 openNav.addEventListener('click', toggleNav)
 exitNav.addEventListener('click', toggleNav)
+
+// GSAP
+
+let defaults = { defaults: { duration: 1 } }
+let timeline = gsap.timeline(defaults)
+
+timeline
+  .from('.p-strapline', { opacity: 0 }, '+=.3')
+  .from('.span-h1', { opacity: 0, x: -200, stagger: .25 }, 0)
+  .from('.li-featured-cabin', { opacity: 0, y: 50, stagger: .25 }, 0)
